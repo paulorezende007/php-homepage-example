a070cac1818ac5735029112103ce9e2b2256bf0d
Paulo Rezende <paulo.rezende@qinetwork.com.br>
2023-09-21 15:36:57 -0300

    update mysql

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,12 +1,12 @@
 <?php
 /* Host name of the MySQL server */
-$host = '10.35.35.3';
+$host = 'DB_HOST';
 /* MySQL account username */
-$user = 'root';
+$user = 'DB_USER';
 /* MySQL account password */
 $passwd = 'DB_PASSWD';#'hF5nb41P6@re.vPH';
 /* The schema you want to use */
-$database = 'website-db';
+$database = 'DB_NAME';
 /* Connection with MySQLi, procedural-style */
 $mysqli = mysqli_connect($host, $user, $passwd, $database);
 /* Check if the connection succeeded */
