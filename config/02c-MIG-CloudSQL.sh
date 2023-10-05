3d033425978355be499eda19e1d5ff5949ee2e63
Paulo Rezende <paulo.rezende@qinetwork.com.br>
2023-10-05 12:11:01 -0300

    green

--- config/02c-MIG-CloudSQL.sh
+++ config/02c-MIG-CloudSQL.sh
@@ -7,7 +7,7 @@ ZONE2="$REGION-b"
 TEMPLATE_NAME=instance-template-5
 NETWORK="vpc-main-01"
 SUBNET="subnet-01"
-STARTUP_SCRIPT_URL="gs://bucket-dgc-arquitetura-tradicional/startup-script-app-cloudsql.sh"
+STARTUP_SCRIPT_URL="gs://bucket-dgc-arquitetura-tradicional/startup-script-mig.sh"
 SERVICE_ACCOUNT_NAME="desvendando-google-cloud-sa"
 HEALTHCHECK_NAME="healthcheck-mig"
 INSTANCE_GROUP_NAME="instance-group-1"
@@ -32,14 +32,14 @@ INSTANCE_GROUP_NAME="instance-group-1"
 
 # Grupo de Instâncias
  gcloud beta compute health-checks create tcp $HEALTHCHECK_NAME --project=$PROJECT_ID --port=80 --proxy-header=NONE \
-  --no-enable-logging --check-interval=10 --timeout=5 --unhealthy-threshold=3 --healthy-threshold=3 
+  --no-enable-logging --check-interval=30 --timeout=20 --unhealthy-threshold=3 --healthy-threshold=3 
  gcloud beta compute instance-groups managed create $INSTANCE_GROUP_NAME --project=$PROJECT_ID --base-instance-name=$INSTANCE_GROUP_NAME \
   --size=2 --template=projects/$PROJECT_ID/global/instanceTemplates/$TEMPLATE_NAME --zones=$ZONE1,$ZONE2 \
   --target-distribution-shape=EVEN --instance-redistribution-type=PROACTIVE --list-managed-instances-results=PAGELESS \
   --health-check=projects/$PROJECT_ID/global/healthChecks/$HEALTHCHECK_NAME --initial-delay=180 --force-update-on-repair
  gcloud compute instance-groups set-named-ports $INSTANCE_GROUP_NAME --project=$PROJECT_ID --region=$REGION --named-ports=http:80
  gcloud beta compute instance-groups managed set-autoscaling $INSTANCE_GROUP_NAME --project=$PROJECT_ID --region=$REGION\
-  --cool-down-period=120 --max-num-replicas=10 --min-num-replicas=2 --mode=on --target-cpu-utilization=0.6
+  --cool-down-period=120 --max-num-replicas=6 --min-num-replicas=2 --mode=on --target-cpu-utilization=0.6
 
  # Create Load Balance
 
--- config/startup-script-mig.sh
+++ config/startup-script-mig.sh
@@ -4,7 +4,7 @@
 if [ -f /root/script_already_executed ]
 then
   echo "*****************************************************************"
-  echo "Execução Cancelada! Este script já foi executado nesta instãncia."
+  echo "Execução Cancelada! Este script já foi executado nesta instância."
   echo "*****************************************************************"
   exit 0
 else
@@ -14,23 +14,34 @@ echo "***********************   START SCRIPT    ***********************"
 echo "*****************************************************************"
 # Environment Variables
 PROJECT_ID='migration-factory-375017'
-DB_HOST='10.35.35.3'
-DB_NAME='website-db'
-DB_USER='root'
-DB_PASSWD=$(gcloud secrets versions access 2 --secret="MYSQL_DB_PASSWD")
+DB_HOST='10.92.234.2'
+DB_NAME='app-db'
+DB_USER='app-user'
+DB_PASSWD=$(gcloud secrets versions access 3 --secret="MYSQL_DB_PASSWD")
+COMMIT=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/commit -H "Metadata-Flavor: Google")
 #
 # Application and Dependences Installation
 apt-get update -y
-apt-get install apache2 php8.1 libapache2-mod-php php-mysql -y
+apt-get install apache2 php8.1 libapache2-mod-php php-mysql mysql-client -y
+#
+# Alterando config do Apache
+sed -i "s/MaxKeepAliveRequests 100/MaxKeepAliveRequests 500/g" /etc/apache2/apache2.conf
 #
 # Deploy WebSite by Git
 cd /var/www/html
 echo "healthy" > healthy.html
 gcloud source repos clone github_paulorezende007_php-homepage-example --project=${PROJECT_ID}
+cd github_paulorezende007_php-homepage-example/
+git checkout $COMMIT
+cd /var/www/html
 mv github_paulorezende007_php-homepage-example/* .
 rm -rf github_paulorezende007_php-homepage-example index.html
 sed -i "s/DB_HOST/${DB_HOST}/g; s/DB_NAME/${DB_NAME}/g; s/DB_USER/${DB_USER}/g; s/DB_PASSWD/${DB_PASSWD}/g" includes/mysql.php
 systemctl restart apache2
+#
+# Install Agente de Monitoramento
+# curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+# bash add-google-cloud-ops-agent-repo.sh --also-install
 #
 # Mark that the script has run before
 touch /root/script_already_executed
